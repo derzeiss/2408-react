f9485baf9e4b14defeaba10a61eaf10af9c3296e
alex-at-dev <123548703+alex-at-dev@users.noreply.github.com>
2024-08-14 12:13:50 +0200

    Add task 05

---
+++ js/05-export.js
@@ -0,0 +1,5 @@
+const createUser = () => 'userCreator';
+export default createUser;
+
+export const printName = () => 'printName';
+export const printAge = () => 'printAge';
---
+++ js/05-main.js
@@ -0,0 +1,3 @@
+import userCreator, { printName as printFirstName, printAge as printDOB } from './05-export.js';
+
+console.log(userCreator(), printFirstName(), printDOB());
