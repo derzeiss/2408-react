0be8293c413ebe03a9d19230fe6d8df7da976460
alex-at-dev <123548703+alex-at-dev@users.noreply.github.com>
2024-08-16 10:45:18 +0200

    Add react task 07

--- react-app/src/components/BookListItem.tsx
+++ react-app/src/components/BookListItem.tsx
@@ -1,5 +1,6 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Book } from '../domain/book/Book';
+import { LikeCounter } from './LikeCounter';
 
 interface BookListItemProps {
   book: Book;
@@ -14,17 +15,20 @@ const getPriceRating = (price: string) => {
 };
 
 export const BookListItem: FC<BookListItemProps> = ({ book }) => {
+  const [likes, setLikes] = useState(0);
   const isFree = book.price === '$0.00';
 
   return (
     <div className={`book-list-item ${isFree ? 'book-list-item_free' : ''}`}>
       <h2>
+        {likes >= 5 && <span className="icon_entry">⭐️</span>}
         {isFree && <span>💰 </span>}
         {book.title}
       </h2>
       <h3>{book.subtitle}</h3>
       {!isFree && <div style={{ color: 'green' }}>{getPriceRating(book.price)}</div>}
       <div className="text-meta">by {book.author}</div>
+      <LikeCounter likes={likes} setLikes={setLikes} />
     </div>
   );
 };
