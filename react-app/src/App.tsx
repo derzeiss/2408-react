82b675d0653bde4e1340584b984c2565b55c1e91
alex-at-dev <123548703+alex-at-dev@users.noreply.github.com>
2024-08-16 12:41:12 +0200

    Add react task 09

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from 'react';
 import { AppHeader } from './components/AppHeader';
 import { BookList } from './components/BookList';
-import { exampleBooks } from './domain/book/exampleBooks';
+import { Book } from './domain/book/Book';
+import { getBooks } from './domain/book/api';
+import { useStateMachine } from './hooks/useStateMachine';
 
 function App() {
+  const { state, next, data: errorMsg } = useStateMachine();
+  const [books, setBooks] = useState<Book[]>([]);
+
+  useEffect(() => {
+    next('loading', 'Loading books...');
+    getBooks()
+      .then((books) => {
+        next('success');
+        setBooks(books);
+      })
+      .catch((err) => {
+        console.error(err);
+        next('error', 'Error while fetching books.');
+      });
+  }, [next]);
+
   return (
     <div className="app">
       <AppHeader />
-      <BookList books={exampleBooks} />
+      {state !== 'success' && errorMsg && <div className="error">{errorMsg}</div>}
+      {state === 'success' && <BookList books={books} />}
     </div>
   );
 }
