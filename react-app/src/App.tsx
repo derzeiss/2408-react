a089afce4448aa15a8f553f9299ccba325ed1ab2
alex-at-dev <123548703+alex-at-dev@users.noreply.github.com>
2024-08-19 10:17:26 +0200

    Add react task 10 useBooks

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,31 +1,13 @@
-import { useEffect, useState } from 'react';
 import { AppHeader } from './components/AppHeader';
 import { BookList } from './components/BookList';
-import { Book } from './domain/book/Book';
-import { getBooks } from './domain/book/api';
-import { useStateMachine } from './hooks/useStateMachine';
+import { useBooks } from './domain/book/useBooks';
 
 function App() {
-  const { state, next, data: errorMsg } = useStateMachine();
-  const [books, setBooks] = useState<Book[]>([]);
-
-  useEffect(() => {
-    next('loading', 'Loading books...');
-    getBooks()
-      .then((books) => {
-        next('success');
-        setBooks(books);
-      })
-      .catch((err) => {
-        console.error(err);
-        next('error', 'Error while fetching books.');
-      });
-  }, [next]);
-
+  const { state, msg, books } = useBooks();
   return (
     <div className="app">
       <AppHeader />
-      {state !== 'success' && errorMsg && <div className="error">{errorMsg}</div>}
+      {state !== 'success' && msg && <div className="error">{msg}</div>}
       {state === 'success' && <BookList books={books} />}
     </div>
   );
---
+++ react-app/src/domain/book/useBooks.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import { useStateMachine } from '../../hooks/useStateMachine';
+import { getBooks } from './api';
+import { Book } from './Book';
+
+export const useBooks = () => {
+  const { state, next, data: msg } = useStateMachine();
+  const [books, setBooks] = useState<Book[]>([]);
+  const [lastRefresh, setLastRefresh] = useState(Date.now());
+
+  const refresh = () => {
+    setLastRefresh(Date.now());
+  };
+
+  useEffect(() => {
+    const refreshInterval = setInterval(refresh, 30 * 1000);
+    return () => clearInterval(refreshInterval);
+  }, []);
+
+  useEffect(() => {
+    next('loading', 'Loading books...');
+    getBooks()
+      .then((books) => {
+        next('success');
+        setBooks(books);
+      })
+      .catch((err) => {
+        console.error(err);
+        next('error', 'Error while fetching books.');
+      });
+  }, [next, lastRefresh]);
+
+  return { state, msg, books };
+};
