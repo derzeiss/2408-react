a9affd05a29ec3d0a28efa4600fbe39a2f70feea
alex-at-dev <123548703+alex-at-dev@users.noreply.github.com>
2024-08-19 12:22:07 +0200

    Support theming for .active nav-link

--- react-app/src/components/AppHeader.tsx
+++ react-app/src/components/AppHeader.tsx
@@ -1,6 +1,12 @@
-import { useContext } from 'react';
+import { CSSProperties, useContext } from 'react';
 import { ThemeContext } from '../domain/theme/ThemeContext';
 import { ThemeEditor } from './ThemeEditor';
+import { NavLink } from 'react-router-dom';
+
+const links = [
+  { path: '/books', text: 'Books' },
+  { path: '/about', text: 'About' },
+];
 
 export const AppHeader = () => {
   const { primaryColor } = useContext(ThemeContext);
@@ -9,6 +15,14 @@ export const AppHeader = () => {
     <div className="app-header">
       <h1 style={{ color: primaryColor }}>Bookmonkey</h1>
       <ThemeEditor />
+
+      <nav style={{ '--c-brand-primary': primaryColor } as CSSProperties}>
+        {links.map((l) => (
+          <NavLink key={l.path} to={l.path}>
+            {l.text}
+          </NavLink>
+        ))}
+      </nav>
     </div>
   );
 };
